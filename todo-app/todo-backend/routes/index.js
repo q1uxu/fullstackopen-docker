3318ac2acec1f5ce6aeceda1c7a35c59e0a5995c
秦秋旭 <qiuxu.qin@outlook.com>
2021-12-08 14:35:57 +0800

    实现一个待办事项计数器：
    第 1 步：每当发送添加待办事项的请求时，将计数器加 1。
    第 2 步：创建一个 GET /statistics 端点，您可以在其中询问使用情况元数据。格式应为以下 JSON：

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
@@ -14,5 +15,10 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  const added_todos = await redis.getAsync('added_todos')
+  res.send({added_todos})
+})
 
 module.exports = router;
